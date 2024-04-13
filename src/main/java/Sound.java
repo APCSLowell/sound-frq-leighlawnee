f23dc7acd630220a39f1c6ebf973e63472755bcb
leighlawnee <143223104+leighlawnee@users.noreply.github.com>
2024-04-12 17:54:48 -0700

    Update Sound.java

--- src/main/java/Sound.java
+++ src/main/java/Sound.java
@@ -1,27 +1,27 @@
 import java.util.*;
 public class Sound
 {
-  /** the array of values in this sound; guaranteed not to be null */
+  /** the array of values in   sound; guaranteed not to be null */
   int[] samples;
 
 
 
-  /** Changes those values in this sound that have an amplitude greater than limit.
+  /** Changes those values in   sound that have an amplitude greater than limit.
    *  Values greater than limit are changed to limit.
    *  Values less than -limit are changed to -limit.
    *  @param limit the amplitude limit
    *         Precondition: limit >= 0
-   *  @return the number of values in this sound that this method changed
+   *  @return the number of values in   sound that   method changed
    */
   public int limitAmplitude(int limit)
   {  
      int count = 0;
-    for(int i = 0; i<this.samples.length; i++){
-        if(this.samples[i]>limit){
-         this.samples[i]=limit;
+    for(int i = 0; i< samples.length; i++){
+        if( samples[i]>limit){
+          samples[i]=limit;
           count++;
-        }else if(this.samples[i]<-limit){
-          this.samples[i]=-limit;
+        }else if( samples[i]<-limit){
+           samples[i]=-limit;
           count++;
         }
     }
@@ -31,7 +31,7 @@ return count;
 
 
 
-  /** Removes all silence from the beginning of this sound.
+  /** Removes all silence from the beginning of   sound.
    *  Silence is represented by a value of 0.
    *  Precondition: samples contains at least one nonzero value
    *  Postcondition: the length of samples reflects the removal of starting silence
@@ -39,16 +39,16 @@ return count;
   public void trimSilenceFromBeginning()
   {
     int i = 0;
-    while (this.samples[i] == 0){
+    while ( samples[i] == 0){
       i++;
     }
-    int [] trimmed = new int [this.samples.length-i];
+    int [] trimmed = new int [ samples.length-i];
     int k = 0;
-    for(int j = i; j<this.samples.length; j++){
-      trimmed[k] = this.samples[j];
+    for(int j = i; j< samples.length; j++){
+      trimmed[k] =  samples[j];
       k++;
     }
-    this.samples = trimmed;
+     samples = trimmed;
   }
 
 }
